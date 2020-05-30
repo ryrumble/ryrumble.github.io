29311cd5ac40f52499181e590bfc4cc67228427f
ryrumble <47289587+ryrumble@users.noreply.github.com>
2020-05-30 00:28:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var gameData = {
   gold: 0,
-  goldPerClick: 1
+  goldPerClick: 1,
+  goldPerClickCost: 10
 }
 
 function mineGold() {
   gameData.gold += gameData.goldPerClick
-  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined" 
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
 }
+
+function buyGoldPerClick() {
+  if (gameData.gold >= gameData.goldPerClickCost) {
+    gameData.gold -= gameData.goldPerClickCost
+    gameData.goldPerClick += 1
+    gameData.goldPerClickCost *= 2
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() {
+  mineGold()
+}, 1000)
